1a69cf9dcf77ef8622cabe1f387d861d33de31ef
YarikDD1 <yarikdd1@gmail.com>
2025-12-15 15:56:17 +0800

    fix: SalonInfo DetachedInstanceError

--- Manictest1.py
+++ Manictest1.py
@@ -316,7 +316,6 @@ async def about_salon(msg: Message):
     async with AsyncSession(engine) as s:
         info = await s.get(SalonInfo, 1)
 
-        # если записи нет — создаём
         if not info:
             info = SalonInfo(
                 id=1,
@@ -325,10 +324,15 @@ async def about_salon(msg: Message):
             s.add(info)
             await s.commit()
 
+        # ✅ сохраняем ПРИМИТИВ
+        salon_text = info.text
+
+    # ⬇️ вне сессии
     await msg.answer(
-        info.text,
+        salon_text,
         reply_markup=reply_kb([["⬅️ Назад"]])
     )
+
 
 
 # ================= WORKS =================
